1f01f5f09ce96f303ab50eb13f73e8f9bee6c40b
lpinsua <luispedroinsua@gmail.com>
2020-07-29 02:22:12 -0600

    Third draft

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,8 @@
-
+#### John's Hopkins University
+#### Data Science Specialization
+#### Getting and Cleaning Data
+#### Final Course Project
+#### Luis Pedro Insua
 
 ### Load required packages
 library(dplyr)
@@ -10,6 +14,7 @@ url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR
 download.file(url, destfile = "data.zip")
 unzip("data.zip")
 
+
 ##### Loading and preparing test data
 
 ## Read complete test table
@@ -18,10 +23,13 @@ testdata <- read.table(".\\UCI HAR Dataset\\test\\X_test.txt")
 testmeans <- rowMeans(testdata)
 teststdevs <- apply(testdata,1,FUN=sd)
 testactivity <- read.table(".\\UCI HAR Dataset\\test\\y_test.txt")
+testsubject <- read.table(".\\UCI HAR Dataset\\test\\subject_test.txt")
 ## Create dataframe with means and stdevs, then give columns friendlier names
-test <- data.frame(testactivity,testmeans,teststdevs)
-names(test)<- c("activity","mean","stdev")
+test <- data.frame(testsubject,testactivity,testmeans,teststdevs)
+names(test)<- c("subject","activity","mean","stdev")
 
+
+##### Loading and preparing train data
 
 ## Read complete train table
 traindata <- read.table(".\\UCI HAR Dataset\\train\\X_train.txt")
@@ -34,7 +42,32 @@ trainsubject <- read.table(".\\UCI HAR Dataset\\train\\subject_train.txt")
 train <- data.frame(trainsubject,trainactivity,trainmeans,trainstdevs)
 names(train)<- c("subject","activity","mean","stdev")
 
-head(train)
-head(test)
 
-rbind(train,test)
+##### 1) Merging taining and test sets into one dataset
+traintest <- rbind(train,test)
+
+
+##### 3) Use descriptive activity names to name the activities in the data set
+
+## Load activity label data
+activitylabels <- read.table(".\\UCI HAR Dataset\\activity_labels.txt")
+names(activitylabels) <- c("code","activityName")
+## Add activity names to the dataframe
+traintest<-left_join(traintest,activitylabels, by=c("activity"="code"))
+head(traintest)
+# Select only necessary variables
+traintest <- select(traintest,activityName,subject, mean, stdev)
+
+
+##### 5) Tidy summary
+
+## Group dataframe by subject and activity
+traintest <- group_by(traintest,activityName,subject)
+## Create summary with averages by activity and subject
+summary <- summarize(traintest,mean(mean),mean(stdev))
+
+
+###### FINAL RESULTS
+head(traintest)
+head(summary)
+
